499499f6a3a09c07ad6e2c78661a2d9d7766e4cd
Geovanny Ortiz <geo.g.ortiz@gmail.com>
2024-08-30 16:40:03 -0400

    Added simple score trackers working on being able to move on to the next question

--- script.js
+++ script.js
@@ -8,8 +8,9 @@ function muteunmute(){
         audio.pause();
     }
 }
-function gamestart(clicked_id){
-    genre = clicked_id;
+function gamestart(clicked_id, clicked_genre){
+    genreid = clicked_id;
+    genre = clicked_genre;
     grid = document.getElementById('grid');
     img = document.getElementById('splashimg');
     quiz = document.getElementById('quiz')
@@ -22,7 +23,7 @@ function gamestart(clicked_id){
         img.style.display = "none";
         quiz.style.display = "block";
         quiz.classList.add('fadeIn');
-        survivalgameloop(genre);
+        survivalgameloop(genreid, genre);
             
     }, 100)
     
@@ -61,19 +62,25 @@ function questionshuffle(answers){
         .sort((a, b) => a.sort - b.sort)
         .map((a) => a.value); 
 }
-function survivalgameloop(genreid){
-    document.getElementById('quizheader').textContent = genreid;
+function questionsetup(genreid,genre){
+
+}
+function survivalgameloop(genreid,genre){
+    document.getElementById('quizheader').textContent = genre;
+
     (async () => {
-        let score = 0;
+        let active = true;
         const tokendata = await fetchAPIData('https://opentdb.com/api_token.php?command=request')
         const token = tokendata.token;
-        const quizdata = await fetchAPIData('https://opentdb.com/api.php?amount=1&token=' + token)
+        const quizdata = await fetchAPIData('https://opentdb.com/api.php?amount=1&token=' + token + "&category=" + genreid)
         const questions = quizdata.results;
         const q = questions[0].question;
         document.getElementById('question').textContent = decodeHtml(q);
         const correct = questions[0].correct_answer;
         const answers = questions[0].incorrect_answers;
         const ansbtns = document.getElementById('answer-buttons');
+        const scorebox = document.getElementById('score');
+        var score = scorebox.textContent;
         answers.push(correct);
         answers.forEach(answer =>{
             const button = document.createElement("button");
@@ -88,25 +95,25 @@ function survivalgameloop(genreid){
                     clicked.style.background = "green";
                     clicked.style.color = "black";
                     score++;
-                    console.log(score);
+                    scorebox.textContent = score;
                 }
                 else{
                     clicked.style.background = "red";
                     clicked.style.color = "black";
-                    nxt.style.display = "block";
-                    console.log(score);
                 }
-                
+                Array.from(ansbtns.children).forEach(button=>{
+                    if(button.textContent == decodeHtml(correct)){
+                        button.style.background = "green";
+                        button.style.color = "black";
+                    }
+                    button.disabled = true;
+                })
+                nxt.style.display = "block";
+                nxt.addEventListener("click", (e) => {
+                    nxt.style.display = "none";
             });
-        
         });
-
-        
     })()    
-}
-function answerclick(e){
-    
-
 }
 function infinitygameloop(genreid){
     document.getElementById('quizheader').textContent = genreid;
